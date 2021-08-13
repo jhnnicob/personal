2970fecdff962166ea06056e2523a8e5e3f07c62
nico <johnnicomolon@yahoo.com>
2021-08-13 14:21:33 +0800

    Add navbar class

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import { useLocation, useRouteMatch } from 'react-router-dom';
 import './Navbar.css';
+import NavItem from './NavItem';
 
 export default function Navbar() {
+    const [show, setShow] = useState(false);
+    const [isActive, setIsActive] = useState(false);    
+    const location = useLocation();
+
+    const match = useRouteMatch();
+
     return (
-        <nav className="navbar">
-            <ul>
-                <li><a href="/">HOME</a></li>
-                <li><a href="/about">ABOUT</a></li>
-                <li><a href="/contact">CONTACT</a></li>
-            </ul>
+        <nav className={`navbar ${show && "navbar__black"}`}>
+                <NavItem to="#home" active={location.hash == "#home" || location.hash == "/"}>HOME</NavItem>
+                <NavItem to="#about" active={location.hash == "#about"}>ABOUT</NavItem>
+                <NavItem to="#projects" active={location.hash == "#projects"}>PROJECTS</NavItem>
+                <NavItem to="#contact" active={location.hash == "#contact"}>CONTACT</NavItem>
         </nav>
     )
 }
