486a6d2e8155a24200433725b4fcfe36e5a5fb2f
nico <johnnicomolon@yahoo.com>
2021-08-14 20:33:38 +0800

    Add reducer

--- src/contactForm/index.js
+++ src/contactForm/index.js
@@ -1,60 +1,129 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import './contactForm.css';
 
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "field":
+            return {
+                ...state,
+                emailContent: {...state.emailContent, [action.name]: action.value}
+            }
+        case "send":
+            return {
+                ...state,
+                isLoading: true,
+            }
+        case "success":
+            return {
+                ...state,
+                successMessage: "Message sent",
+                isLoading: false,
+                emailContent: {
+                    name: '',
+                    email: '',
+                    subject: '',
+                    message: '',
+                }
+            }
+        case "error":
+            return {
+                ...state,
+                error: "Somthing went wrong",
+                isLoading: false,
+                emailContent: {
+                    name: '',
+                    email: '',
+                    subject: '',
+                    message: '',
+                }
+            }
+        default:
+            return state;
+    }
+}
+
 export default function ContactForm() {
 
-    const [status, setStatus] = useState("Submit");
+    const initilaState = {
+        isLoading: false,
+        error: '',
+        successMessage: '',
+        emailContent: {
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, initilaState);
+    const {emailContent, isLoading, error, successMessage} = state;
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setStatus("Sending...");
-        const { name, email, subject, message } = e.target.elements;
-        let details = {
-            name: name.value,
-            email: email.value,
-            subject: subject.value,
-            message: message.value
-        };
-        console.log(details);
+        dispatch({type: "send"}) ;
+
         let response = await fetch("http://localhost:5000/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8",
             },
-            body: JSON.stringify(details),
+            body: JSON.stringify(emailContent),
         });
-        console.log(response);
-        setStatus("Submit");
-        let result = await response.json();
-        alert(result.status);
+
+        try {
+            await response.json();
+            dispatch({type: "success"});
+        } catch (error) {
+            dispatch({type: error});
+        }
     };
 
+    const handleChange = (e) => {
+        dispatch({
+            type: "field",
+            name: [e.target.name],
+            value: e.target.value
+        });
+    }
+
+    console.log(emailContent);
     return (
         <div className="contactForm">
             <form onSubmit={handleSubmit}>
+                <p>{successMessage}</p>
                 <h2>Leave a Message</h2>
                 <div className="contactForm__control">
                     <div className="contactForm__row">
                         <TextField 
                             label="Your name" 
                             name="name"
+                            value={emailContent.name}
+                            onChange={handleChange}
                             fullWidth/>
                         <TextField 
                             label="Email" 
                             name="email"
+                            value={emailContent.email}
+                            onChange={handleChange}
                             fullWidth/>
                     </div>
                     <div className="contactForm__row">
                         <TextField 
                             label="Subject" 
                             name="subject"
+                            value={emailContent.subject}
+                            onChange={handleChange}
                             fullWidth/>
                     </div>
                     <div className="contactForm__row">
                         <TextField 
                             label="Message" 
                             name="message"
+                            value={emailContent.message}
+                            onChange={handleChange}
                             fullWidth/>
                     </div>
                     <div className="contactForm__row">
@@ -62,8 +131,9 @@ export default function ContactForm() {
                             type="submit"
                             className="contactForm__button" 
                             variant="contained" 
-                            color="primary">
-                            Send a message
+                            color="primary"
+                            disabled={isLoading}>
+                            {isLoading ? "Loading..." : "Send a message"}
                         </Button>
                     </div>
                 </div>
