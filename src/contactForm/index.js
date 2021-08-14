e9e421e0513e5c77205d88eaf099e70220d557e5
nico <johnnicomolon@yahoo.com>
2021-08-14 13:47:12 +0800

    Add form element

--- src/contactForm/index.js
+++ src/contactForm/index.js
@@ -6,35 +6,38 @@ import './contactForm.css';
 export default function ContactForm() {
     return (
         <div className="contactForm">
-            <h2>Leave a Message</h2>
-            <div className="contactForm__control">
-                <div className="contactForm__row">
-                    <TextField 
-                        label="Your name" 
-                        fullWidth/>
-                    <TextField 
-                        label="Email" 
-                        fullWidth/>
+            <form action="mailto:johnnicomolon@yahoo.com" method="post" encrype="text/plain">
+                <h2>Leave a Message</h2>
+                <div className="contactForm__control">
+                    <div className="contactForm__row">
+                        <TextField 
+                            label="Your name" 
+                            fullWidth/>
+                        <TextField 
+                            label="Email" 
+                            fullWidth/>
+                    </div>
+                    <div className="contactForm__row">
+                        <TextField 
+                            label="Subject" 
+                            fullWidth/>
+                    </div>
+                    <div className="contactForm__row">
+                        <TextField 
+                            label="Message" 
+                            fullWidth/>
+                    </div>
+                    <div className="contactForm__row">
+                        <Button
+                            type="submit"
+                            className="contactForm__button" 
+                            variant="contained" 
+                            color="primary">
+                            Send a message
+                        </Button>
+                    </div>
                 </div>
-                <div className="contactForm__row">
-                    <TextField 
-                        label="Subject" 
-                        fullWidth/>
-                </div>
-                <div className="contactForm__row">
-                    <TextField 
-                        label="Message" 
-                        fullWidth/>
-                </div>
-                <div className="contactForm__row">
-                    <Button
-                        className="contactForm__button" 
-                        variant="contained" 
-                        color="primary">
-                        Send a message
-                    </Button>
-                </div>
-            </div>
+            </form>
         </div>
     )
 }
