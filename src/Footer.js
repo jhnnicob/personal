6b47ede55b53e2e905ccb83ddc93a0cde6a92a26
nico <johnnicomolon@yahoo.com>
2021-08-16 18:02:58 +0800

    Add Footer component

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+        <span>Nico Molon © — 2021</span>
+        </footer>
+    )
+}
