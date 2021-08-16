7aa3dbc1780e325a3f79e4870bf2f515ca576532
nico <johnnicomolon@yahoo.com>
2021-08-16 18:03:52 +0800

    Add useEffect for eventListener-scroll

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,33 @@
-import React,{useState, useEffect} from 'react';
-import { useLocation, useRouteMatch } from 'react-router-dom';
+import React,{useState, useEffect, history} from 'react';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
 import './Navbar.css';
 import NavItem from './NavItem';
 
-export default function Navbar() {
-    const [show, setShow] = useState(false);
-    const [isActive, setIsActive] = useState(false);    
-    const location = useLocation();
+export default function Navbar() { 
 
-    const match = useRouteMatch();
+    const location = useLocation();
+    const history = useHistory();
+
+    useEffect(() => {
+        window.addEventListener("scroll", () =>{
+            if(window.scrollY === 0  && window.scrollY < 338) {
+                history.push("");
+            }else if(window.scrollY > 338 && window.scrollY < 619){
+                history.push("#about");
+            }else if(window.scrollY > 619 && window.scrollY < 1982) {
+                history.push("#projects");
+            }else if(window.scrollY > 1982 && window.scrollY < 2449) {
+                history.push("#contact");
+            }
+            return () => {
+                window.removeEventListener("scroll");
+            }
+        })
+    }, [])
 
     return (
-        <nav className={`navbar ${show && "navbar__black"}`}>
-                <NavItem to="#home" active={location.hash == "#home" || location.hash == "/"}>HOME</NavItem>
+        <nav className={"navbar"}>
+                <NavItem to="#home" active={location.hash == "#home" || location.hash == ""}>HOME</NavItem>
                 <NavItem to="#about" active={location.hash == "#about"}>ABOUT</NavItem>
                 <NavItem to="#projects" active={location.hash == "#projects"}>PROJECTS</NavItem>
                 <NavItem to="#contact" active={location.hash == "#contact"}>CONTACT</NavItem>
