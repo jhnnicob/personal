dc50d649d2f9e97b44e04edd3dbf4a366d10d02d
nico <johnnicomolon@yahoo.com>
2021-08-17 10:28:29 +0800

    Add download button

--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,4 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 import './Header.css';
 import Navbar from './Navbar';
@@ -9,6 +10,7 @@ export default function Header() {
                 <Navbar />
                 <h1>Hello I'm <span className="myName">Nico Molon</span></h1>
                 <p>Web Developer</p>
+                <Button variant="outlined" color="primary" href="../resume\JohnNico_Molon_Resume.PDF" download>Download my resume</Button>
             </div>
         </div>
     )
